9ddd6609d8e61c26c49f7fc4faf497a0c2c43fb3
Aadil Ahmad <axa1819@student.bham.ac.uk>
2023-03-28 15:29:33 +0100

    added WebSocketConfiguration and auth for community

--- src/main/webapp/app/entities/community/community-routing.module.ts
+++ src/main/webapp/app/entities/community/community-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommunityComponent } from './community.component';
+import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
 
-const routes: Routes = [{ path: '', component: CommunityComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: CommunityComponent,
+    canActivate: [UserRouteAccessService],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
