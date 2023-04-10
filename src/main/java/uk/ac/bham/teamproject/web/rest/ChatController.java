b305fa40cf7548ec15f8f68752aedee319041e26
Aadil Ahmad <axa1819@student.bham.ac.uk>
2023-04-10 17:31:57 +0100

    edited design and added database save for controle

--- src/main/java/uk/ac/bham/teamproject/web/rest/ChatController.java
+++ src/main/java/uk/ac/bham/teamproject/web/rest/ChatController.java
@@ -1,19 +1,28 @@
 package uk.ac.bham.teamproject.web.rest;
 
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import uk.ac.bham.teamproject.domain.ChatMessage;
+import uk.ac.bham.teamproject.repository.ChatMessageRepository;
 
 @Controller
 public class ChatController {
+
+    private final ChatMessageRepository chatMessageRepository;
+
+    public ChatController(ChatMessageRepository chatMessageRepository) {
+        this.chatMessageRepository = chatMessageRepository;
+    }
 
     @MessageMapping("/topic")
     @SendTo("/topic/{roomId}")
     public ChatMessage handleChatMessage(@Payload ChatMessage message) {
         // Return the saved message to all subscribers of the /topic/{roomId} destination
+
+        // save message in database
+        chatMessageRepository.save(message);
 
         message.getRoom().getId();
 
