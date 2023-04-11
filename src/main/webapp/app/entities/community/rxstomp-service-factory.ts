44b9ac92dbbb1f9f3837ac8dc70e8f01f0b355be
Kris <kr.jakubonis@gmail.com>
2023-04-11 07:04:25 +0100

    Reverted some changes

--- src/main/webapp/app/entities/community/rxstomp-service-factory.ts
+++ src/main/webapp/app/entities/community/rxstomp-service-factory.ts
@@ -3,9 +3,8 @@ import { myRxStompConfig } from './rxstomp.config';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function rxStompServiceFactory() {
-  /*const rxStomp = new RxStompService();
+  const rxStomp = new RxStompService();
   rxStomp.configure(myRxStompConfig);
   rxStomp.activate();
   return rxStomp;
-  */
 }
