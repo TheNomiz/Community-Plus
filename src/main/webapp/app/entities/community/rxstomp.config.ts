40c6978caad25076fb3073728aef90654acdcb93
Aadil Ahmad <axa1819@student.bham.ac.uk>
2023-04-30 19:42:12 +0100

    removed debug for stompclient

--- src/main/webapp/app/entities/community/rxstomp.config.ts
+++ src/main/webapp/app/entities/community/rxstomp.config.ts
@@ -24,9 +24,8 @@ export const myRxStompConfig: RxStompConfig = {
   // Will log diagnostics on console
   // It can be quite verbose, not recommended in production
   // Skip this key to stop logging to console
-  // eslint-disable-next-line object-shorthand
-  debug: (msg: string): void => {
-    // eslint-disable-next-line no-console
-    console.log(new Date(), msg);
-  },
+  //debug: (msg: string): void => {
+  // eslint-disable-next-line no-console
+  //  console.log(new Date(), msg);
+  //},
 };
